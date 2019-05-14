4766d07de715a3069621a5a3029fc9680afff723
dmitrijMzr <dmtr.ma@gmail.com>
2019-05-14 19:36:07 +0300

    style

--- src/js/header/Settings/Settings.jsx
+++ src/js/header/Settings/Settings.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-
+import './Setting.less'
 
 export default class Settings extends Component {
 
@@ -7,12 +7,8 @@ export default class Settings extends Component {
     render() {
         return (
             <div className="header-settings_wrapper">
-                <button
-                    className="header-settings_button"
-                    onClick={() => console.log("settings")}
-                    children={"Settings"}
-                />
                 <div className="header-settings_modalWindow">
+                    <div>
                     <button
                         className="modalWindow_buttonLang"
                         onClick={() => console.log("Eng")}
@@ -23,12 +19,17 @@ export default class Settings extends Component {
                         onClick={() => console.log("Ru")}
                         children={"Ru"}
                     />
+                    </div>
+                    <div>
                     <span className="modalWindow_span">Server msg</span>
                     <input type="checkbox" className="modalWindow_toggler" />
+                    </div>
+                    <div>
                     <input
                         type="text"
                         className="modalWindow_userBlacklist" />
                     <input type="checkbox" className="modalWindow_blacklistCheckbox" />
+                    </div>
                 </div>
             </div>
         )
